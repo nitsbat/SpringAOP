2c20e3fc9de0427c185e562315b5542943daae80
Nitin Bisht <3498georgian@gmail.com>
2021-03-30 21:42:40 +0530

    Pointcut and wildcard expressions

--- src/org/bisht/JavaAspect.java
+++ src/org/bisht/JavaAspect.java
@@ -2,12 +2,22 @@ package org.bisht;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class JavaAspect {
 
-    @Before("execution(public String org.bisht.model.Triangle.getName())")
+    @Before("allGetters()")
     public void loggingAdvice() {
         System.out.println("Logging advice. calling getMEthod");
     }
+
+    @Before("allGetters()")
+    public void transactionAdvice() {
+        System.out.println("Transaction advice.");
+    }
+
+    @Pointcut("execution(* org.bisht.model.*.get*(..))")
+    public void allGetters(){}
+
 }
